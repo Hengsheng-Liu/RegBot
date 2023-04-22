e0839c2c0c43e3425da5342922fb399619f89f2d
Hengsheng-Liu <lhs7714@bu.edu>
2023-04-21 21:22:20 -0400

    try put timefunction insdei eval:

--- index.js
+++ index.js
@@ -1,69 +1,108 @@
 const puppeteer = require('puppeteer');
+const fs = require('fs');
+const credentials = loadCredentials();
+
 (async () => {
-  const email = process.argv[2];
-  const password = process.argv[3];
+  const email = credentials.username;
+  const password = credentials.password;
   const planner_option = "reg%2Fadd%2Fbrowse_schedule.pl&SearchOptionDesc=Planner&SearchOptionCd=P&ViewSem=Fall+2023&KeySem=20243&AddPlannerInd="
-  const cookie_name = "f5_cspm=1234"
   if (!email || !password) {
-    console.error('Usage: node index.js [email] [password]');
+    console.error('Username or password not provided in secrets.json');
     process.exit(1);
   }
-
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
-  // row-label push-label 
-  // Navigate to Boston University login page
   await page.goto('https://student.bu.edu');
   await page.waitForNavigation({ waitUntil: 'networkidle0' }),
-  await login(page, email, password);
-  await page.waitForNavigation({ waitUntil: 'networkidle0' }),
-  // Navigate to the course registration page
-  await page.waitForSelector('.community_navigation-tileMenuItemBanner_tileMenuItemBanner.comm-tile-menu__item-title.vertical-padding.slds-text-align_center');
-
-  // Click on the planner element and wait for navigation
-  const planner = await page.waitForSelector('.community_navigation-tileMenuItemBanner_tileMenuItemBanner.comm-tile-menu__item-title.vertical-padding.slds-text-align_center');
-  //await planner.click();
-  //await page.waitForNavigation({ waitUntil: 'networkidle0' });
-  await Promise.all([
-     // After clicking the submit
-    planner.click(),
-    page.waitForNavigation({ waitUntil: 'networkidle0' }),
-  ]);
-  const cookie = await page.cookies() 
-  console.log(cookie)
-  await page.waitForTimeout(1000);
-  const base_url = page.url();
-  const planner_Url = base_url.split('?')[0] + '?ModuleName='+planner_option ;
-  await page.goto(planner_Url);
-  await page.$$eval('tr[align="center"]', (elements) => {
-    elements.forEach((element) => {
-      const checkbox = element.querySelector('input[type="checkbox"]');
-      if (checkbox) {
-        checkbox.click(); 
-      }
-    });
-  });
-  const submitElement = await page.$('[value="Add Classes to Schedule"]');
-  /*
-  page.on('dialog', async dialog => {
-    await dialog.accept();
-  });
-  */ 
-  await submitElement.click();
-  //console.log('tr elements with align="center" containing a checkbox:', trElementsWithCheckbox);
-  // Close the browser
-  //await browser.close();
+    await login(page, email, password);
+  await GoToRegPage(page, planner_option);
+  await signUp(page);
 })();
-
+function loadCredentials() {
+  const data = fs.readFileSync('secrets.json', 'utf8');
+  return JSON.parse(data);
+}
+function getCurrentTimestamp() {
+  const now = new Date();
+  const date = now.toLocaleDateString();
+  const time = now.toLocaleTimeString();
+  return date + ' ' + time;
+}
+function printClass(td){
+  const class_code = tdArray[2].textContent.trim();
+  const seat = tdArray[5].textContent.trim();
+  const class_with_professor = tdArray[3].innerText;
+  const [class_name, class_professor] = class_with_professor.split("\n");
+  const class_type = tdArray[7].textContent.trim();
+  const class_building = tdArray[8].textContent.trim();
+  const class_room = tdArray[9].textContent.trim();
+  const class_date = tdArray[10].textContent.trim();
+  const class_start_time = tdArray[11].textContent.trim();
+  const class_end_time = tdArray[12].textContent.trim();
+  console.log("Class: " + class_code + " " + class_name + " " + class_type + " " + class_professor
+    + " " + class_date + " " + class_building + " " + class_room + " " + class_start_time + " " + class_end_time + " " + seat + " seats open" + "at " + getCurrentTimestamp());
+}
 async function login(page, email, password) {
   page.waitForNavigation(),
-  await page.type('#j_username', email);
-  await page.type('#j_password', password); 
+    await page.type('#j_username', email);
+  await page.type('#j_password', password);
   await Promise.all([
     page.waitForNavigation(),
     page.keyboard.press('Enter'),
   ]);
 }
-async function registerCourses(page) {
-  // Implement course registration logic
+async function GoToRegPage(page, planner_option) {
+  await page.waitForNavigation({ waitUntil: 'networkidle0' }),
+    await page.waitForSelector('.community_navigation-tileMenuItemBanner_tileMenuItemBanner.comm-tile-menu__item-title.vertical-padding.slds-text-align_center');
+  const planner = await page.waitForSelector('.community_navigation-tileMenuItemBanner_tileMenuItemBanner.comm-tile-menu__item-title.vertical-padding.slds-text-align_center');
+  await Promise.all([
+    planner.click(),
+    page.waitForNavigation({ waitUntil: 'networkidle0' }),
+  ]);
+  await page.waitForTimeout(1000);
+  const base_url = page.url();
+  const planner_Url = base_url.split('?')[0] + '?ModuleName=' + planner_option;
+  await page.goto(planner_Url);
 }
+
+async function signUpOnce(page) {
+  const checkboxExists = await page.$('input[type="checkbox"]');
+  if (!checkboxExists) {
+    console.log(getCurrentTimestamp()+ " " +"No classes available");
+    return;
+  }
+  await page.$$eval('tr[align="center"]', (trElements) => {
+    const trArray = [];
+    trElements.forEach((trElement) => {
+      trArray.push(trElement);
+    });
+    trArray.forEach((trElement) => {
+      const tdArray = Array.from(trElement.querySelectorAll('td'));
+      const checkbox = trElement.querySelector('input[type="checkbox"]');
+      if (checkbox) {
+        checkbox.click();
+        printClass(tdArray);
+      }
+    });;
+  },printClass);
+  ;
+  const submitElement = await page.$('[value="Add Classes to Schedule"]');
+  await submitElement.click();
+  /*
+  page.on('dialog', async dialog => {
+    await dialog.accept();
+  });
+  */
+}
+
+async function signUp(page) {
+  while (true) {
+    const waitTime = credentials.waitTimeInSeconds * 1000; 
+    await signUpOnce(page);
+    await page.waitForTimeout(waitTime); // Replace 10000 with the desired waiting time in milliseconds
+    await page.reload({ waitUntil: 'networkidle0' }); // Refresh the page
+  }
+}
+
+
+
